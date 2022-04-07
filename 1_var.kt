0108f5a92034e98831cbf509bd648f09dbf966be
Miyka96 <micaelaseminara@outlook.it>
2022-04-07 19:18:22 +0200

    concat

--- 1_var.kt
+++ 1_var.kt
@@ -1,9 +1,23 @@
 fun main() {
-  // dichiaro var e relativo val ☀️
+
+  // dichiaro var e relativo val -----------
   var todaysDate: String = "04/07/2022"
   println (todaysDate)
-  // dichiaro var vuota, poi assegno val
+
+  // dichiaro var vuota, poi assegno val---------
   var currentWeather: String
   currentWeather= "nuvoloso"
   println(currentWeather)
+
+  // VAL (costante)----------
+  val pi: Double = 3.14
+ 
+  // concatenare------------
+  val dog = "Toto"
+  val state = "Kansas"
+  val movie = "The Wizard of Oz"
+
+  var concat= dog + ", I've a feeling we're not in " +
+  state+" anymore. " + movie + ", 1939"
+  print(concat)
 }
