0b7ce2c86d4a052092a13a61df34b89d4f00fecd
Miyka96 <micaelaseminara@outlook.it>
2022-04-07 18:55:58 +0200

    var firsts

---
+++ 1_var.kt
@@ -0,0 +1,9 @@
+fun main() {
+  // dichiaro var e relativo val ☀️
+  var todaysDate: String = "04/07/2022"
+  println (todaysDate)
+  // dichiaro var vuota, poi assegno val
+  var currentWeather: String
+  currentWeather= "nuvoloso"
+  println(currentWeather)
+}
